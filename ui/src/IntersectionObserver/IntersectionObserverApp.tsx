988b076d1f7ac603f298b9f2740b4bd2b593b633
ShinEun9 <ses2201@naver.com>
2023-08-08 11:51:40 +0900

    0808 Intersection Observer로 무한스크롤 구현 연습

--- ui/src/IntersectionObserver/IntersectionObserverApp.tsx
+++ ui/src/IntersectionObserver/IntersectionObserverApp.tsx
@@ -1,7 +1,97 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import useIntersectionObserver from "./useIntersectionObserver";
+
+interface Airline {
+  id: Number;
+  name: String;
+  country: String;
+  logo: String;
+  slogan: String;
+  head_quearters: String;
+  website: String;
+  established: String;
+}
+
+interface Passenger {
+  _id: String;
+  name: String;
+  trips: Number;
+  airline: Airline;
+  __v: number;
+}
+
+interface Props {
+  isLastItem: boolean;
+  onFetchMorePassengers: () => void;
+  children: React.ReactNode;
+}
+const Passenger: React.FC<Props> = ({
+  isLastItem,
+  onFetchMorePassengers,
+  children,
+}) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(ref, {});
+  const isIntersecting = !!entry?.isIntersecting;
+
+  useEffect(() => {
+    isLastItem && isIntersecting && onFetchMorePassengers();
+  }, [isLastItem, isIntersecting]);
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        border: "1px dashed #000",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
 function IntersectionObserverApp() {
-  return <div>IntersectionObserverApp</div>;
+  const [page, setPage] = useState<number>(0);
+  const [passengers, setPassengers] = useState<Array<Passenger>>([]);
+  const [isLast, setIsLast] = useState<boolean>(false);
+  const getPassengers = async () => {
+    const params = { size: 10, page };
+    try {
+      const res = await axios.get(
+        "https://api.instantwebtools.net/v1/passenger",
+        { params }
+      );
+
+      const passengers = res.data.data;
+      const isLast = res.data.totalPages === page;
+
+      setPassengers((prev) => [...prev, ...passengers]);
+      setIsLast(isLast);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    !isLast && getPassengers();
+  }, [page]);
+
+  return (
+    <div>
+      {passengers.map((passenger, idx) => (
+        <Passenger
+          key={String(passenger._id)}
+          isLastItem={passengers.length - 1 === idx}
+          onFetchMorePassengers={() => setPage((prev) => prev + 1)}
+        >
+          {passenger.name}
+        </Passenger>
+      ))}
+    </div>
+  );
 }
 
 export default IntersectionObserverApp;
